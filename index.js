97cfd4816c6bba2b0e3e3bee35898b235bb1dbda
Pratham kumar <prathamsolera@gmail.com>
2020-06-04 10:10:35 +0530

    Dark mode

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+var isClick = true;
 
 function handleClick() {
-  document.body.style.background = "#1c1c1c";
-  document.getElementsByTagName("h1")[0].style.webkitTextStroke = "0px black";
+  if (isClick) {
+    document.body.style.background = "#1c1c1c";
+    document.getElementsByTagName("h1")[0].style.webkitTextStroke = "2px #e1e1e1";
+    var y = document.getElementsByTagName("P");
+    var i;
+    for (i = 0; i < y.length; i++) {
+      y[i].style.color = "#bcbcbc";
+    }
+    isClick = false;
+  } else {
+    document.body.style.background = "#fff";
+    document.getElementsByTagName("h1")[0].style.webkitTextStroke = "2px black";
+    var y = document.getElementsByTagName("P");
+    var i;
+    for (i = 0; i < y.length; i++) {
+      y[i].style.color = "#545454";
+    }
+    isClick = true;
+  }
+
 }
